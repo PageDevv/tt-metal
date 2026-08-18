bec3b6875ccd9ab301e8d7de0a2a29ffc762dfa9
Oleksandr Bacherikov <obacherikov@tenstorrent.com>
2026-08-18 01:31:48 +0000

    Global Semaphore and CircularBuffer creation update

--- ttnn/core/global_semaphore.cpp
+++ ttnn/core/global_semaphore.cpp
@@ -4,19 +4,18 @@
 
 #include "ttnn/global_semaphore.hpp"
 
-#include <tt-metalium/host_api.hpp>
 #include <tt-metalium/global_semaphore.hpp>
 
 namespace ttnn::global_semaphore {
 
 GlobalSemaphore create_global_semaphore(
     IDevice* device, const CoreRangeSet& cores, uint32_t initial_value, BufferType buffer_type) {
-    return CreateGlobalSemaphore(device, cores, initial_value, buffer_type);
+    return GlobalSemaphore(device, cores, initial_value, buffer_type);
 }
 
 GlobalSemaphore create_global_semaphore(
     MeshDevice* mesh_device, const CoreRangeSet& cores, uint32_t initial_value, BufferType buffer_type) {
-    return CreateGlobalSemaphore(mesh_device, cores, initial_value, buffer_type);
+    return GlobalSemaphore(*mesh_device, cores, initial_value, buffer_type);
 }
 
 tt::tt_metal::DeviceAddr get_global_semaphore_address(const GlobalSemaphore& global_semaphore) {
